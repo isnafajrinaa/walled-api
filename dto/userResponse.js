bce83da4e2cc319b0b31ef6f4714e28f347146eb
isnafajrinaa <fajrinaisna@student.ub.ac.id>
2024-12-18 16:24:39 +0700

    first commit

---
+++ dto/userResponse.js
@@ -0,0 +1,17 @@
+class UserResponse {
+  constructor(user) {
+    this.id = user.id;
+    this.username = user.username;
+    this.fullname = user.fullname;
+    this.email = user.email;
+    this.avatar_url = user.avatar_url;
+    if (user.wallet) {
+      this.wallet = {
+        account_number: user.wallet.account_number,
+        balance: user.wallet.balance,
+      };
+    }
+  }
+}
+
+module.exports = { UserResponse };
